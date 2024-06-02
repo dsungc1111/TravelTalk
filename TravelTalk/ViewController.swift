45ea54738deb2fc4eee1c2d49c26d580a46ec8f1
dsungc1111 <eotjd9808@naver.com>
2024-06-02 14:31:52 +0900

    Initial Commit

---
+++ TravelTalk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TravelTalk
+//
+//  Created by 최대성 on 6/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
